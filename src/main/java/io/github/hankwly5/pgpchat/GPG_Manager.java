e9892559c0d467284bbd263a9373ff0af5aaf918
hankwly5 <hankwly5@protonmail.com>
2026-06-03 21:31:39 -0400

    Add file untracked by git

---
+++ src/main/java/io/github/hankwly5/pgpchat/GPG_Manager.java
@@ -0,0 +1,26 @@
+package io.github.hankwly5.pgpchat;
+
+import java.io.File;
+
+public class GPG_Manager {
+
+    public static String GPG_Path() {
+        String[] possible_paths = {
+            "/usr/bin/gpg2",
+            "/usr/local/bin/gpg2",
+            "/opt/homebrew/bin/gpg2",
+            "C:/Program Files (x86)/GnuPG/bin/gpg.exe",
+			"/usr/bin/gpg",
+			"/usr/local/bin/gpg",
+			"/opt/homebrew/bin/gpg"
+        };
+
+        for (String path : possible_paths) {
+            if (new File(path).exists()) {
+                return path;
+            }
+        }
+
+        return null;
+    }
+}
